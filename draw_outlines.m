ecfdbbdbe4bdbd400bc8eb5953ab8a472332da83
susmit31 <gohanmysticdbz@gmail.com>
2021-01-31 19:27:18 +0600

    Vectorized draw_outlines.m

--- draw_outlines.m
+++ draw_outlines.m
@@ -1,15 +1,29 @@
 function newimg = draw_outlines(img, keep_top = .25, num_cols = 1, varargin )
-  img_copy = [ zeros(1, size(img)(2)+2);
-zeros(size(img)(1),1) img zeros(size(img)(1),1);
-zeros(1, size(img)(2)+2) ];
+  [im_ht,im_wd] = size(img);
+  % Adding some padding to the image
+  img_copy = [ zeros(1, im_wd+2);
+  zeros(im_ht,1) img zeros(im_ht,1);
+  zeros(1, im_wd+2) ];
+  
+  % Convolving with the given filters
   convd_img = multi_conv(img_copy, varargin{:})(1:end-1,1:end-1);
+  [convd_ht ,convd_wd] = size(convd_img);
+  
   thresholds  = zeros(1,num_cols);
   n_colour = zeros(size(convd_img));
-  for i=1:num_cols,
-    thresholds(i) = quantile( quantile( convd_img, 1 - i*keep_top/num_cols ), 1 - i*keep_top/num_cols );
-    n_colour += (convd_img>thresholds(i));
-  end
+  
+  % Calculating the quantiles, followed by thresholds for each quantile
+  qts = 1 - (1:num_cols)*keep_top/num_cols;
+  thresholds = quantile(quantile(convd_img, qts), qts, 2);
+  thresholds = diag(thresholds,0);
+  
+  % Calculating the brightness grading of pixels
+  n_colour = reshape(convd_img, convd_ht*convd_wd, 1) < thresholds';
+  n_colour = sum(n_colour,2);
+  n_colour = reshape(n_colour, convd_ht, convd_wd);
+  
+  % Rescaling the graded array followed by converting it to uint8
   n_colour = cast(n_colour,'double')*255/num_cols;
-  colourflip_two_colour = -cast(n_colour,'double')+255;
-  newimg = cast(colourflip_two_colour,'uint8');
+  newimg = cast(n_colour,'uint8');
+
 end
